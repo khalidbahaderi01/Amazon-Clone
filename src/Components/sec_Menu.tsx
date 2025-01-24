fde29c175b81d609c3d9010f5792fbe55bbc737e
wares <mw@tibxtech.com>
2025-01-24 15:53:29 +0430

    added sec_Menu

--- src/Components/sec_Menu.tsx
+++ src/Components/sec_Menu.tsx
@@ -1,25 +1,25 @@
 import '../Theme/style.css'
+import {menuItems} from "../Data/StaticData"
 
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 export default function SecondaryMenu(){
+
+    const location = useLocation()
+
     return(
         <div className="menuBar">
             <nav className="menuItems">
-
+            <a href="" className="Menu_link" ><i className="fa-solid fa-bars bar"></i></a>
+            <a href="">
             
-            <a href="" className="Menu_link"><i className="fa-solid fa-bars bar"></i><Link to={"/All"} className="Menu_link" > All </Link></a>
+            {menuItems.map((item) => (
+                <Link to={item.path} className={`Menu_link menu  ${
+                location.pathname === item.path ? "active" : ""}`}> {item.name} </Link>
+            ))}
             
-
-
-            
-        
-        
-            <a href="" className="Menu_link">Today's Deal</a>
-            <a href="" className="Menu_link">Customer Service</a>
-            <a href="" className="Menu_link">Registery</a>
-            <a href="" className="Menu_link">Gift Cards</a>
-            <a href="" className="Menu_link">Sell</a>
+            </a>
+          
             </nav>
         </div>
     ); 
---
+++ src/Data/StaticData.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+    {name: "All", path: "/all"},
+    {name: "Today's Deal", path: "/today_deal"},
+    {name: "Customer Service", path: "/customer_services"},
+    {name: "Registery", path: "/registry"},
+    {name: "Gift Cards", path: "/gift_cards"},
+    {name: "Sell", path: "/sell"},
+]
